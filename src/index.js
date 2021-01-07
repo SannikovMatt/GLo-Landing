894c16f21bbf0e1746ec3142fc6164b6e584cd55
SannikovMatt <sannikov.matt@gmail.com>
2021-01-07 19:02:00 +0100

    Carousel Ready

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import team from './modules/team';
 import slider from './modules/slider';
 import calculator from './modules/calculator';
 import sendForm from './modules/sendForm';
+import SliderCarusel from './modules/carousel';
 
 //Timer
 countTimer('2021/12/31');
@@ -19,6 +20,26 @@ togglePopUp();
 tabs();
 //Слайдер
 slider();
+//Карусель
+const options = {
+
+    main: ".companies-wrapper",
+    wrap: ".companies-hor",
+    prev: '#test-left',
+    next: '#test-right',
+    slidesToShow: 4,
+    infinity: true,
+    responsive:
+        [
+            { breakpoint: 1024, slidesToShow: 3 },
+            { breakpoint: 768, slidesToShow: 2 },
+            { breakpoint: 576, slidesToShow: 1, }
+        ]
+};
+
+const carousel = new SliderCarusel(options);
+
+carousel.init(); 
 //Команда 
 team();
 //Калькулятор
